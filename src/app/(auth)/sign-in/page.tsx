23f21c7de6fac4a70963153cf491527a05d7f579
Anshul <anshulbhaskar50@gmail.com>
2025-06-02 10:15:22 +0530

    04-Authentication-UI

---
+++ src/app/(auth)/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Card } from '@/components/ui/card';
+import { SignInView } from '@/app/modules/auth/ui/views/sign-in-view';
+
+const Page = () => {
+    return <SignInView/>;
+}
+
+export default Page;
---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,8 @@
+import { SignUpView } from "@/app/modules/auth/ui/views/sign-up-view";
+import { Card } from "@/components/ui/card";
+
+const Page = () => {
+    return <SignUpView/>
+}
+
+export default Page;
